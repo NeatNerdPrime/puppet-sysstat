7b24453381f3515187062532e2be05cda2973cc2
Matthew Mallard <matt@Q-Technologies.com.au>
2017-08-03 12:06:35 +1000

    Fixed some code alignment issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,21 +14,21 @@ class sysstat(
   String $sa2_minute,
 
   # Class parameters are populated from External(hiera)/Defaults/Fail
-  String    $sa1_options   = '-S ALL',
-  Integer   $interval = 2,
-  Integer   $sa1_interval  = $interval,
-  Integer   $duration = 2,
-  Integer   $sa1_duration  = $duration,
-  Integer   $samples = 1,
-  Integer   $sa1_samples   = $samples,
-  String    $sa2_options   = '-A',
-  Integer   $history       = 10,
-  Integer   $compressafter = 31,
-  String    $sadc_options  = '-S DISK',
-  String    $zip           = 'bzip2',
-  String    $installpkg    = 'yes',
+  String    $sa1_options      = '-S ALL',
+  Integer   $interval         = 2,
+  Integer   $sa1_interval     = $interval,
+  Integer   $duration         = 2,
+  Integer   $sa1_duration     = $duration,
+  Integer   $samples          = 1,
+  Integer   $sa1_samples      = $samples,
+  String    $sa2_options      = '-A',
+  Integer   $history          = 10,
+  Integer   $compressafter    = 31,
+  String    $sadc_options     = '-S DISK',
+  String    $zip              = 'bzip2',
+  String    $installpkg       = 'yes',
   String    $generate_summary = 'yes',
-  String    $disable       = 'no',
+  String    $disable          = 'no',
 ) {
   # Convert duration to seconds
   $sa1_duration_seconds = $sa1_duration * 60 - 1
@@ -42,17 +42,17 @@ class sysstat(
   file { $cron_path:
     ensure  => $ensure,
     content => epp('sysstat/crontab.epp', {
-      sa1_path         => $sa1_path,
-      sa1_options      => $sa1_options,
+      sa1_path             => $sa1_path,
+      sa1_options          => $sa1_options,
       sa1_interval         => $sa1_interval,
       sa1_duration         => $sa1_duration,
       sa1_duration_seconds => $sa1_duration_seconds,
       sa1_samples          => $sa1_samples,
-      sa2_path         => $sa2_path,
-      sa2_options      => $sa2_options,
-      sa2_minute       => $sa2_minute,
-      sa2_hour         => $sa2_hour,
-      generate_summary => $generate_summary,
+      sa2_path             => $sa2_path,
+      sa2_options          => $sa2_options,
+      sa2_minute           => $sa2_minute,
+      sa2_hour             => $sa2_hour,
+      generate_summary     => $generate_summary,
     }),
   }
 
@@ -60,9 +60,8 @@ class sysstat(
     package { $package:
       ensure => installed,
     }
-  } ->
-
-  file { $conf_path:
+  }
+  -> file { $conf_path:
     ensure  => file,
     content => epp('sysstat/sysconfig.epp', {
       history       => $history,
